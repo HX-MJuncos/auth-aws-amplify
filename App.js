c4df3f857adfdf2ec2a190eceba12dee26590482
Carlos Martin Juncos <mjuncos@soyhenry.com>
2023-03-16 17:28:18 -0300

    160320231728

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import  {Amplify,  Auth } from 'aws-amplify';
+import awsConfig from './src/aws-exports.js';
+import { withAuthenticator } from 'aws-amplify-react-native';
 
-export default function App() {
+Amplify.configure(awsConfig)
+
+const App = () => {
+
+  async function salir() {
+    try {
+      await Auth.signOut()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text tyle={styles.text} >Welcome!</Text>
+      <TouchableOpacity onPress={salir} >
+        <Text style={styles.buttonText} >SingOut</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+export default withAuthenticator(App)
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +34,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonText: {
+    textAlign: 'center',
+    padding: 20,
+    color: 'red'
+  }
 });
